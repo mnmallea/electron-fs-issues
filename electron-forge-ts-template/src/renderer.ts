119a3da906cdf48494012fa320764584f2fca7e4
mnmallea <mnmallea@gmail.com>
2020-07-25 17:07:22 -0300

    Added read results to html

--- electron-forge-ts-template/src/renderer.ts
+++ electron-forge-ts-template/src/renderer.ts
@@ -1,9 +1,13 @@
 import * as fs from 'fs';
 
+const $placeholder = document.getElementById('placeholder') as HTMLElement;
+
 console.log('Reading file..');
 fs.promises.readFile('tini.csv')
     .then(content => {
         console.log('Done');
-        console.log(content.toString());
+        const stringContent = content.toString();
+        console.log(stringContent);
+        $placeholder.innerText = stringContent;
     })
     .catch(console.error);
